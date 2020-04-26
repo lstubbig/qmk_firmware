d7ffe06187ddcc268954365ca73d9e7e3152f61b
Leon S <leonstubbig@web.de>
2020-04-26 13:56:09 +0200

    Added my DZ60 config

---
+++ keyboards/dz60/keymaps/lstubbig/config.h
@@ -0,0 +1,15 @@
+// define mouse behavior
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
+// define tapping layer toggle
+#define TAPPING_TOGGLE 3
+
+// backlight keymap_config
+#undef BACKLIGHT_LEVELS
+#define BACKLIGHT_LEVELS 8
+// # define BACKLIGHT_BREATHING
+// # define BREATHING_PERIOD 6
