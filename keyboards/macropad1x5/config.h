1254089dcc03d7aac3d915570595486aebf26355
Leon S <leonstubbig@web.de>
2020-10-30 20:44:12 +0100

    Macropad1x5 first commit

---
+++ keyboards/macropad1x5/config.h
@@ -0,0 +1,48 @@
+/*
+Copyright 2020 Leon Stubbig <leonstubbig@web.de>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x0801
+#define PRODUCT_ID      0x1133
+#define DEVICE_VER      0x0100
+#define MANUFACTURER    Leon Stubbig
+#define PRODUCT         Macropad1x5
+#define DESCRIPTION     1x5 Macropad with Rotary Encoder
+
+/* key matrix size */
+#define MATRIX_ROWS 1
+#define MATRIX_COLS 5
+
+/* Keyboard Matrix Assignments */
+#define DIRECT_PINS { \
+    { D2, B6, B2, B3, B1} \
+}
+
+#define ENCODERS_PAD_A { D1 }
+#define ENCODERS_PAD_B { D0 }
+
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
